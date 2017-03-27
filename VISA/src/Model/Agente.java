a40169b76893d164f3033160f680de471e50933d
DESKTOP-GF9ISI0\Alison <isidroevc@gmail.com>
2017-03-27 15:14:33 -0600

    Cambios, estoy haciendo la lectura de los datos Recuerda poner las vistas en el paquete View

--- VISA/src/Model/Agente.java
+++ VISA/src/Model/Agente.java
@@ -1,13 +1,25 @@
 package Model;
 
+import java.io.File;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Agente extends Persona {
     private int id;
     private Date fechaIngreso;
     private Date fechaNacimiento;
     private float salario;
+    private String password;
     
+    //Constructores
+    public Agente(){
+        
+    }
+    
+    public Agente(int id,Date fechaIngreso, Date fechaNacimiento ){
+        
+    }
+    //Métodos de acceso
     public void setId(int id){
         this.id=id;
     }
@@ -39,5 +51,32 @@ public class Agente extends Persona {
         return salario;
     }
     
-    
-}
+    //-Métodos estáticos 
+     
+     //Método para buscar un agente por su id
+     
+     public static Agente buscarAgente(int id){
+         //
+         String[] registro;
+         boolean encontrado = false;
+         try{
+            Scanner lector = new Scanner(new File("src/Resources/Agentes.txt"));
+            while(lector.hasNextLine()){
+                registro = lector.nextLine().split(",");
+                //Si el id es igual al id solicitado
+                if(registro[0].trim().equals(Integer.toString(id))){
+                    encontrado = true;
+                    break;
+                }
+            }
+            //Cerrar el flujo del archivo
+            lector.close();
+         }catch(Exception ex){
+             
+         }
+         //Si si se encontró
+         if(encontrado){
+            return null;
+         }
+         return null;
+     }
